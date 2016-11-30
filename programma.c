ec1895e132dd90bea5452a9dcd58db7d1cc7183d
stage <stage@debian.cst>
2016-11-30 17:50:56 +0100

    Aggiunto il primo file C

---
+++ programma.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int somma(int a,int b){
+return a+b;
+}
+int main(){
+int i,j;
+scanf("%d",&i); 
+scanf("%d",&j);
+printf("%d\n",somma(i,j));
+
+//system("pause");
+getchar();getchar();
+}
+
+
